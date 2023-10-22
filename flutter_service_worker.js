b2c5743c732c255519acf37466acebcc0bba348a
Guilherme Santana FCT_MIEI <gf.santana@campus.fct.unl.pt>
2023-10-22 18:34:06 +0100

    project stage 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "66da7e3bc6ed726c20ff48911c78208a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2b86282514130171026a8ddea538dac7",
-"/": "2b86282514130171026a8ddea538dac7",
-"main.dart.js": "b0933f17382c8e71ec61e32f5faf927d",
+"index.html": "266e76df7dfc76cf3681479fb2d948c1",
+"/": "266e76df7dfc76cf3681479fb2d948c1",
+"main.dart.js": "387c9a358fbd6813667890edd0440ea9",
 "manifest.json": "54ee97732a9f5afe005eff2a87c220a5",
 "version.json": "af11ad529707a73e83ee85d195f3f9e6"};
 // The application shell files that are downloaded before a service worker can
