d3f90e4902ee47fd5a3fed9097c1c37a0a2113fa
mafaldalami <m.lami@campus.fct.unl.pt>
2023-11-21 23:40:44 +0000

    recent version with bug in plan trip

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,50 +3,54 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {
-  "assets/AssetManifest.bin": "2d9a97d0df98a8c40a7cfb705cfc8022",
-  "assets/AssetManifest.json": "18502db413a4aaf080b54d14cfb2742e",
-  "assets/assets/foto%2520e%2520texto.png": "f68bdcd1e34c8970766e8b51b4a152e4",
-  "assets/assets/foto.png": "9227cd20d4d492ad4bfc8689a7679a45",
-  "assets/assets/noimage.png": "8de1717e2bc0d204b2be3cce0115ebf5",
-  "assets/assets/pata.png": "d849038e7dba458e895bac5baa036f5d",
-  "assets/assets/texto.png": "d497b9efaee6b725bf6d511159ba2f56",
-  "assets/assets/travel1.jpg": "b1a8b639bc85222a7c74e6d999280455",
-  "assets/assets/travel2.jpg": "d17ad6dbd82a4b30197d8eadb58eb60d",
-  "assets/assets/travel3.jpg": "3d56bea1bb36b6d96959b9f78a62605b",
-  "assets/assets/travel4.jpg": "0bf3adf66163bc466a0316b775a65c9c",
-  "assets/assets/Trip.png": "0569b7daad18b9e6e7d6c15f439e3c09",
-  "assets/assets/user.json": "237521bc2938192b311a4490a2405c68",
-  "assets/assets/user.png": "5863a2256a85311f3e0d20fa9947da80",
-  "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-  "assets/fonts/MaterialIcons-Regular.otf": "137e88d79823f63e6eb34ce2a1a20462",
-  "assets/NOTICES": "3358c9beab3beb23d4dee3620044d028",
-  "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
-  "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
-  "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
-  "canvaskit/canvaskit.js": "bbf39143dfd758d8d847453b120c8ebb",
-  "canvaskit/canvaskit.wasm": "42df12e09ecc0d5a4a34a69d7ee44314",
-  "canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee22aebb0",
-  "canvaskit/chromium/canvaskit.wasm": "be0e3b33510f5b7b0cc76cc4d3e50048",
-  "canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
-  "canvaskit/skwasm.wasm": "1a074e8452fe5e0d02b112e22cdcf455",
-  "canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15",
-  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-  "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-  "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-  "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-  "index.html": "f76c136d21dfa2e9160aed08b9e27fa6",
-  "/": "f76c136d21dfa2e9160aed08b9e27fa6",
-  "main.dart.js": "3bbe03ed69644aeb959f463c9c45675c",
-  "manifest.json": "54ee97732a9f5afe005eff2a87c220a5",
-  "version.json": "af11ad529707a73e83ee85d195f3f9e6"
-};
+const RESOURCES = {"version.json": "af11ad529707a73e83ee85d195f3f9e6",
+"index.html": "8b5e37e4e0ead005be97123f05ec8f75",
+"/": "8b5e37e4e0ead005be97123f05ec8f75",
+"main.dart.js": "e51f618462d50a9db54519d923d4f95d",
+"flutter.js": "7a1d76a8729d1add7a6ce2a4b4db043f",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "611a7d398f28bbd6146d9e905d0c7cf1",
+"assets/AssetManifest.json": "18502db413a4aaf080b54d14cfb2742e",
+"assets/NOTICES": "ceee5cb4317fd85e66b9845b6d60f2ae",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/AssetManifest.bin.json": "7750f0439cd552560932959a5b9f47dd",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
+"assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
+"assets/shaders/ink_sparkle.frag": "4096b5150bac93c41cbc9b45276bd90f",
+"assets/AssetManifest.bin": "2d9a97d0df98a8c40a7cfb705cfc8022",
+"assets/fonts/MaterialIcons-Regular.otf": "360132b924fa5306d1b48a4be2ee5ff5",
+"assets/assets/travel3.jpg": "3d56bea1bb36b6d96959b9f78a62605b",
+"assets/assets/travel2.jpg": "d17ad6dbd82a4b30197d8eadb58eb60d",
+"assets/assets/travel1.jpg": "b1a8b639bc85222a7c74e6d999280455",
+"assets/assets/texto.png": "d497b9efaee6b725bf6d511159ba2f56",
+"assets/assets/travel4.jpg": "0bf3adf66163bc466a0316b775a65c9c",
+"assets/assets/user.png": "5863a2256a85311f3e0d20fa9947da80",
+"assets/assets/foto%2520e%2520texto.png": "f68bdcd1e34c8970766e8b51b4a152e4",
+"assets/assets/user.json": "3fc3050a571af57134e7352db1727dba",
+"assets/assets/foto.png": "9227cd20d4d492ad4bfc8689a7679a45",
+"assets/assets/Trip.png": "0569b7daad18b9e6e7d6c15f439e3c09",
+"assets/assets/pata.png": "d849038e7dba458e895bac5baa036f5d",
+"assets/assets/noimage.png": "8de1717e2bc0d204b2be3cce0115ebf5",
+"canvaskit/skwasm.js": "411f776c9a5204d1e466141767f5a8fa",
+"canvaskit/skwasm.js.symbols": "36607a151127e3736083d241f6e7356b",
+"canvaskit/canvaskit.js.symbols": "34eb740000df15c03210028f34bc9cf5",
+"canvaskit/skwasm.wasm": "045364c77c9eedecbd12d2c77fe8fa0a",
+"canvaskit/chromium/canvaskit.js.symbols": "afe994e4d3d6d23db1d643573c9839ce",
+"canvaskit/chromium/canvaskit.js": "bc979fce6b4b3cc75d54b0d162cafaa7",
+"canvaskit/chromium/canvaskit.wasm": "1ec8ac7ed8ea5906c2e03fc14cb2c22a",
+"canvaskit/canvaskit.js": "321aa0c874f6112cabafc27a74a784b4",
+"canvaskit/canvaskit.wasm": "2778fe4a13eac805b37df04590085ba3",
+"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
-  "index.html",
-  "assets/AssetManifest.bin.json",
-  "assets/FontManifest.json"];
+"index.html",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
 
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
@@ -54,15 +58,15 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value, { 'cache': 'reload' })));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
-self.addEventListener("activate", function (event) {
-  return event.waitUntil(async function () {
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
     try {
       var contentCache = await caches.open(CACHE_NAME);
       var tempCache = await caches.open(TEMP);
@@ -143,7 +147,7 @@ self.addEventListener("fetch", (event) => {
     return onlineFirst(event);
   }
   event.respondWith(caches.open(CACHE_NAME)
-    .then((cache) => {
+    .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
         // lazily populate the cache only if the resource was successfully fetched.
